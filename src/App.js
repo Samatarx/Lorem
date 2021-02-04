d3a82b4aaeb6976fde19c9e37fa8f901de5f5cbd
Samatar Xasan <Samatar.Xasan@gmail.com>
2021-02-04 14:19:53 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import data from "./data";
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let number = parseInt(count);
+
+    if (number <= 0) {
+      number = 1;
+    }
+    if (number > data.length) {
+      number = data.length;
+    }
+    setText(data.slice(0, number));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="section-center">
+        <h3>Tired of Boring Lorem Ipsum?</h3>
+        <form className="lorem-form" onSubmit={handleSubmit}>
+          <label htmlFor="amount">Paragraphs:</label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button className="btn">Generate</button>
+        </form>
+        <article className="lorem-text">
+          {text.map((para, index) => {
+            return <p key={index}>{para}</p>;
+          })}
+        </article>
+      </section>
+    </main>
   );
 }
 
